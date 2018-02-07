fabf3162910a330ef9337a272321bdddb0f85334
Matt <4009945+MattBDev@users.noreply.github.com>
2018-02-07 17:55:15 -0500

    Update test_combinations.adb

--- hw1/ada/test_combinations.adb
+++ hw1/ada/test_combinations.adb
@@ -2,31 +2,31 @@
 with Ada.Text_IO;  use Ada.Text_IO;
  
 procedure Test_Combinations is
-   generic
-      type Integers is range <>;
-   package Combinations is
+   generic 
+      type Integers is range <>; //Integers I believe implies that it will be more than one, making it a list
+   package Combinations is //basically an interface for Combinations which is implemented below.
       type Combination is array (Positive range <>) of Integers;
-      procedure First (X : in out Combination);
+      procedure First (X : in out Combination); // in out means X can be modified and read
       procedure Next (X : in out Combination); 
-      procedure Put (X : Combination);
+      procedure Put (X : Combination); //put only allows reading since in is default when not listed
    end Combinations;
  
    package body Combinations is
       procedure First (X : in out Combination) is
       begin
-         X (1) := Integers'First;
+         X (1) := Integers'First; // 
          for I in 2..X'Last loop
             X (I) := X (I - 1) + 1;
          end loop;
       end First;
       procedure Next (X : in out Combination) is
       begin
-         for I in reverse X'Range loop
+         for I in reverse X'Range loop //start loop
             if X (I) < Integers'Val (Integers'Pos (Integers'Last) - X'Last + I) then
                X (I) := X (I) + 1;
                for J in I + 1..X'Last loop
                   X (J) := X (J - 1) + 1;
-               end loop;
+               end loop; //end loop
                return;
             end if;
          end loop;
@@ -45,7 +45,7 @@ procedure Test_Combinations is
    use Fives;
  
    X : Combination (1..3);
-begin
+begin  //start the 
    First (X);
    loop
       Put (X); New_Line;
